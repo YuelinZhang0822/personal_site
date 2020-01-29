ab5cbdd5c146bd23605732fb1185e5bb39ac33c8
Yuelin Zhanng <zyl960822-@tamu.edu>
2020-01-29 15:44:51 -0800

    Add status, created, updated columns to Wish model

--- models/wish.py
+++ models/wish.py
@@ -6,4 +6,12 @@ class Wish(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(1024), nullable=False)
     description = db.Column(db.String, nullable=True)
+    status = db.Column(db.String, nullable=False)
+    created = db.Column(db.DateTime, server_default=db.func.now())
+    updated = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+    def __init__(self, title, description='', status='active'):
+        self.title=title
+        self.description=description
+        self.status=status
 
