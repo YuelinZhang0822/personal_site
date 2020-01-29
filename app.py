d03b587e72e30260fb770e3a5f19bc9c8bdd0ae8
Yuelin Zhanng <zyl960822-@tamu.edu>
2020-01-28 18:33:51 -0800

    First template
    base.html
    index.html
    template inherience

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask import render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
@@ -9,6 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/devdb'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+bootstrap = Bootstrap(app)
 
 
 @app.route("/", methods=["GET"])
@@ -16,5 +19,5 @@ def show_wish_list():
     # Import Wish here to avoid import looping
     from personal_site.models.wish import Wish
     wishes = Wish.query.all()
-    fomated_wishes = [{"id": wish.id, "title": wish.title, "description": wish.description} for wish in wishes]
-    return fomated_wishes[0]["title"]
+    formated_wishes = [{"id": wish.id, "title": wish.title, "description": wish.description} for wish in wishes]
+    return render_template('index.html', wishes=formated_wishes)
