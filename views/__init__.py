7d4285a1a7b8ec7b851134a9b31917f7524ec88f
Yuelin Zhanng <zyl960822-@tamu.edu>
2020-01-31 17:09:11 -0800

    some frontend change

--- views/__init__.py
+++ views/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import absolute_import
 
 import personal_site.views.index
+import personal_site.views.wish
--- views/index.py
+++ views/index.py
@@ -5,8 +5,5 @@ from personal_site.managers.wish_manager import wish_manager as wm
 
 
 @app.route("/", methods=["GET"])
-def show_wish_list():
-    # Import Wish here to avoid import looping
-    all_wishes = wm.get_all()
-    wishes = wm.many_to_dict(all_wishes)
-    return render_template('index.html', wishes=wishes)
+def show_homepage():
+    return render_template('index.html')
