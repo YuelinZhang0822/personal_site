6f2d1d0992911ee99648a7dc8bbd5b5154f85cb7
Yuelin Zhanng <zyl960822-@tamu.edu>
2020-03-09 00:05:47 -0700

    Image browser frontend

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -29,7 +29,37 @@ $(document).ready(function() {
         const file_node = $("#image-selector");
         const file_path = file_node.val();
         const file_name = file_path.replace("C:\\fakepath\\", "");
-        $("#selected-image-label").text(file_name)
+        const preview = $("#image-preview");
+        $("#selected-image-label").text(file_name);
+
+        const file = file_node.prop('files')[0];
+        const fileReader = new FileReader();
+        fileReader.onload = function () {
+            // convert image file to base64 stringc
+
+            const uploaded_data = fileReader.result;
+            preview.attr("src", uploaded_data);
+        };
+        fileReader.readAsDataURL(file)
+    });
+
+    $("#image-selector-addon").click(function () {
+       const uploaded_data = $("#image-preview").attr('src');
+       const data = JSON.stringify({'image_base64': uploaded_data});
+       $.ajax({
+           url: 'image/add',
+           type: 'POST',
+           dataType: 'json',
+           data: data,
+           contentType: 'application/json',
+           success: function () {
+               alert("upload image successfully!")
+           },
+           error: function () {
+               alert("failed")
+           }
+       });
+
     });
 
 });
